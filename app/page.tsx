cc7417a20090c860d6fdeecd44543fffc0594b95
Arnav Shukla <87523233+thelordzeus@users.noreply.github.com>
2024-05-27 19:33:17 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import {
+  Header,
+  Hero,
+  About,
+  Skills,
+  Testimonials,
+  Projects,
+  Contact,
+  Footer
+} from '@/components/sections';
+
+import Preloader from '@/components/preloader/preloader';
+import Cursor from '@/components/cursor/cursor';
+import SmoothScroll from '@/components/smooth-scroll';
+
+export default function Home() {
+  return (
+    <SmoothScroll>
+      <Preloader />
+      <div className="flex min-h-[100dvh] flex-col">
+        <Header />
+        <main className="flex-1">
+          <Hero />
+          <About />
+          <Projects />
+          <Skills />
+          <Testimonials />
+          <Contact />
+          <Footer />
+        </main>
+      </div>
+      <Cursor />
+    </SmoothScroll>
+  );
+}
